13ff88dc69653ceca0c1d33524c6d3fb458d6b14
Chet Gnegy <chetgnegy@gmail.com>
2013-12-02 23:39:49 -0800

    Hopefully cleaned up some things

--- CollideFx.cpp
+++ CollideFx.cpp
@@ -19,20 +19,22 @@
 #include "Physics.h"
 #include "Menu.h"
 
+#define CLASSDEMO 0
+
 
 int main(int argc, char *argv[]) {
-    srand (time(NULL));
+  srand (time(NULL));
 
-
+  if (!CLASSDEMO){
     Graphics *myGraphics = new Graphics(1100, 600);
     myGraphics->initialize(argc, argv);
-
+    
     myGraphics->show_splash_screen();
 
     UGenChain *myChain = new UGenChain();
     myChain->initialize_audio();
     myChain->initialize_midi();
-
+    
     Menu *myMenu = new Menu();
     World *myWorld = new World(30, 30, 9, 0);
 
@@ -45,9 +47,67 @@ int main(int argc, char *argv[]) {
     Graphics::add_drawable(myWorld, 2);
     Physics::set_bounds(30*(1-2*World::kWallThickness), 30*(1-2*World::kWallThickness), 9, 0);
     myGraphics->start_graphics();
-      
+  
+  }
+  else {
+    Graphics *myGraphics = new Graphics(960, 600);
+    myGraphics->initialize(argc, argv);
+    
 
-    return 1;
+    UGenChain *myChain = new UGenChain();
+    myChain->initialize_audio();
+    myChain->initialize_midi();
+    
+    Menu *myMenu = new Menu();
+    World *myWorld = new World(30, 30, 9, 0);
+
+
+    Graphics::add_moveable(myMenu);
+    myMenu->link_ugen_graph(myChain->get_signal_graph());
+    if (UGenChain::has_midi()){ myMenu->enable_midi(); }
+
+
+    Input *i = new Input();
+    Reverb *c = new Reverb();
+    Delay *d = new Delay();
+    
+    Disc *inp = new Disc(i, 1.5, false);
+    Disc *chor = new Disc(c, 1.5, false);
+    Disc *dist = new Disc(d, 1.5, false);
+    
+    chor->set_texture(6);    
+    dist->set_texture(7);
+            
+    Graphics::add_drawable(inp);
+    Graphics::add_moveable(inp);
+    Physics::give_physics(inp);
+    Graphics::add_drawable(chor);
+    Graphics::add_moveable(chor);
+    Physics::give_physics(chor);
+    Graphics::add_drawable(dist);
+    Graphics::add_moveable(dist);
+    Physics::give_physics(dist);
+
+    myChain->get_signal_graph()->add_input(inp);
+    myChain->get_signal_graph()->add_effect(chor);
+    myChain->get_signal_graph()->add_effect(dist);
+
+    inp->set_location(0,3);
+    chor->set_location(2,-3);
+    dist->set_location(0,0);
+    inp->set_color(0.7, 0.7, 0.3);
+    chor->set_color(0.2, 0.7, 0.3);
+    dist->set_color(0.0, 0.7, 0.3);
+    
+   
+    
+    Graphics::add_drawable(myWorld, 2);
+    Physics::set_bounds(30*(1-2*World::kWallThickness), 30*(1-2*World::kWallThickness), 9, 0);
+    myGraphics->start_graphics();
+  
+  }
+  
+  return 1;
 
 }
 
